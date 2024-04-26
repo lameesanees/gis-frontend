2bad39968b3c26827af14c68c98a6b3396d9f132
Lamees Anees <lamees.anees@gmail.com>
2024-04-26 23:12:35 +0530

    26

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -16,14 +16,37 @@ function Home() {
           <source src={video1} type="video/mp4" />
         </video>
         <div className="overlay-text position-absolute top-50 start-50 translate-middle text-center">
-          <h2 className="video-caption">Streamlined Reporting</h2>
+          <h2 className="video-caption indian-flag-text">
+            Welcome to &nbsp;
+            <span>G</span>
+            <span>U</span>
+            <span>A</span>
+            <span>R</span>
+            <span>D</span>
+            <span>&nbsp;</span>
+            <span>I</span>
+            <span>N</span>
+            <span>D</span>
+            <span>I</span>
+            <span>A</span>
+            <span>&nbsp;</span>
+            <span>S</span>
+            <span>E</span>
+            <span>V</span>
+            <span>A</span>
+            <span>&nbsp;</span>
+    !
+          </h2>
           <p className="video-description">
             Efficiently report accidents and file cases with our user-friendly
             platform.
           </p>
           <Nav className="d-flex justify-content-center">
             <Link to="/login">
-              <Button className="btn" style={{ backgroundColor: "black", color: "white" }}>
+              <Button
+                className="btn"
+                style={{ backgroundColor: "black", color: "white" }}
+              >
                 <FaUserPlus className="me-2 text-white" />
                 Get Started
               </Button>
