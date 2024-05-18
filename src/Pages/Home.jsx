207bc5cd39e4e7e77a30dca90f15bc71eefc23ff
Lamees Anees <lamees.anees@gmail.com>
2024-05-18 12:33:47 +0530

    btn

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -55,11 +55,22 @@ function Home() {
           <Nav className="d-flex justify-content-center">
             {isLoggedIn ? (
               <Link to="/dashboard">
-                <button
-                  className="btn"
-                  style={{ backgroundColor: "#354a33", color: "white" }}
-                >
+                <button class="cssbuttons-io-button">
                   Track your application
+                  <div class="icon">
+                    <svg
+                      height="11"
+                      width="11"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M0 0h24v24H0z" fill="none"></path>
+                      <path
+                        d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"
+                        fill="currentColor"
+                      ></path>
+                    </svg>
+                  </div>
                 </button>
               </Link>
             ) : (
